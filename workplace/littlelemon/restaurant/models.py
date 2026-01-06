b7fa8737e8f95885484d19c5e3c0461b653b3576
Francisco <fescalona4@gmail.com>
2026-01-06 14:13:03 -0500

    first part complete

--- workplace/littlelemon/restaurant/models.py
+++ workplace/littlelemon/restaurant/models.py
@@ -13,3 +13,10 @@ class Booking(models.Model):
 
 
 # Add code to create Menu model
+class Menu(models.Model):
+   name = models.CharField(max_length=200)
+   price = models.IntegerField()
+   menu_item_description = models.TextField(max_length=1000, default=' ')
+
+   def __str__(self):
+      return self.name
